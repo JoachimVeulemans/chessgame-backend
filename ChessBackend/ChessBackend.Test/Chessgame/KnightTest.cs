eaae21ce78ea61abadd9ad157ea58cb5532ef41c
Ian Angillis <ianangillis@gmail.com>
2019-08-30 20:12:09 +0200

    Unit test for chesspieces

---
+++ ChessBackend/ChessBackend.Test/Chessgame/KnightTest.cs
@@ -0,0 +1,43 @@
+﻿using ChessBackend.Services.ChessGame.Src.Entities;
+using ChessBackend.Services.ChessGame.Src.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace ChessBackend.Test.Chessgame
+{
+    public class KnightTest
+    {
+        private readonly Knight _sut;
+        public KnightTest()
+        {
+            _sut = new Knight(Color.WHITE);
+        }
+
+        [Fact]
+        public void Knight_ShouldHaveBlackColorOnCreation()
+        {
+            //Arrange
+            var sut = new Knight(Color.BLACK);
+
+            //Assert
+            Assert.Equal(Color.BLACK, sut.Color);
+        }
+
+        [Fact]
+        public void Knight_ShouldHaveWhiteColorOnCreation()
+        {
+            //Assert
+            Assert.Equal(Color.WHITE, _sut.Color);
+        }
+
+        [Fact]
+        public void Knight_ShouldHaveCorrectValueAndPGNOnCreation()
+        {
+            //Assert
+            Assert.Equal(3, _sut.Value);
+            Assert.Equal("N", _sut.GetPGN());
+        }
+    }
+}
---
+++ ChessBackend/ChessBackend.Test/Chessgame/PawnTest.cs
@@ -0,0 +1,43 @@
+﻿using ChessBackend.Services.ChessGame.Src.Entities;
+using ChessBackend.Services.ChessGame.Src.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace ChessBackend.Test.Chessgame
+{
+    public class PawnTest
+    {
+        private readonly Pawn _sut;
+        public PawnTest()
+        {
+            _sut = new Pawn(Color.WHITE);
+        }
+
+        [Fact]
+        public void King_ShouldHaveBlackColorOnCreation()
+        {
+            //Arrange
+            var sut = new Pawn(Color.BLACK);
+
+            //Assert
+            Assert.Equal(Color.BLACK, sut.Color);
+        }
+
+        [Fact]
+        public void King_ShouldHaveWhiteColorOnCreation()
+        {
+            //Assert
+            Assert.Equal(Color.WHITE, _sut.Color);
+        }
+
+        [Fact]
+        public void King_ShouldHaveCorrectValueAndPGNOnCreation()
+        {
+            //Assert
+            Assert.Equal(1, _sut.Value);
+            Assert.Equal("P", _sut.GetPGN());
+        }
+    }
+}
