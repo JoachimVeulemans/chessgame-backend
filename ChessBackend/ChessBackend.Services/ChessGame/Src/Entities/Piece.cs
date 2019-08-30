d37ef579b709d7192b79b1f98c1e8aaef4c4a932
Ian Angillis <ianangillis@gmail.com>
2019-08-30 19:35:14 +0200

    Chessgame - added enum and chesspieces

---
+++ ChessBackend/ChessBackend.Services/ChessGame/Src/Entities/Piece.cs
@@ -0,0 +1,27 @@
+﻿using ChessBackend.Services.ChessGame.Src.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChessBackend.Services.ChessGame.Src.Entities
+{
+    public abstract class Piece
+    {
+        public string Position { get; set; }
+        public int Value { get; set; }
+        public ChessPiece ChessPiece { get; set; }
+        public Color Color { get; set; }
+
+        public Piece(Color color, ChessPiece chessPiece, int value)
+        {
+            Color = color;
+            ChessPiece = ChessPiece;
+            Value = value;
+        }
+
+        public virtual string GetPGN()
+        {
+            return ChessPiece.ToString()[0] + "";
+        }
+    }
+}
---
+++ ChessBackend/ChessBackend.Services/ChessGame/Src/Enums/Color.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChessBackend.Services.ChessGame.Src.Enums
+{
+    public enum Color
+    {
+        BLACK,
+        WHITE
+    }
+}
