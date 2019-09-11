5be68b9917d52f7e224296e7882efb056d780f49
Ian Angillis <ianangillis@gmail.com>
2019-09-11 17:58:24 +0200

    ApplicationUtilities for useful functions

---
+++ ChessBackend/ChessBackend/Entities/ApplicationUtilities.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace ChessBackend.Entities
+{
+    public static class ApplicationUtilities
+    {
+        public static string GetUserIdFromHttpContext(HttpContext httpContext)
+        {
+            return httpContext.User.Claims.Where(c => c.Type == ClaimTypes.NameIdentifier).Select(p => p.Value).FirstOrDefault();
+        }
+    }
+}
