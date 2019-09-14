9866bf6095d11c65d19cc385f6b1e9a9a17d8c85
Ian Angillis <ianangillis@gmail.com>
2019-09-14 20:16:25 +0200

    refactor

---
+++ ChessBackend/ChessBackend/Controllers/UsersController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ChessBackend.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChessBackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IUserService _userService;
+        public UsersController(IUserService userService)
+        {
+            _userService = userService;
+        }
+        
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return Ok(await _userService.GetAllAsync());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(string id)
+        {
+            return Ok(await _userService.GetByIdAsync(id));
+        }
+    }
+}
